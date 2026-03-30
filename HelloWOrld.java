986792729be1cdcfd11a2d3c7ebe1c1568d90888
Santiago <sromerocely@gmail.com>
2026-03-30 11:44:48 -0400

    George Washington change
    added a name and age

--- HelloWOrld.java
+++ HelloWOrld.java
@@ -3,5 +3,13 @@ public class HelloWOrld
    public static void main(String[] args)
    {
       System.out.println("Hello World!");
+      
+      String name = "George Washington";
+      
+      System.out.println(name);
+      
+      int age = 24;
+      
+      System.out.println(age);
       }
 }
